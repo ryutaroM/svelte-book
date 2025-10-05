922fc88d7c48ec1453333e7f9bea9f535103cc7b
ryutaroM <arcticmonkey326@gmail.com>
2025-10-05 20:36:41 +0900

    init commit

---
+++ .github/chatmodes/custom ask.chatmode.md
@@ -0,0 +1,11 @@
+---
+description: 'Custom Ask for svelte projects'
+model: Claude Sonnet 4.5 (Preview) (copilot)
+tools: ['edit', 'runNotebooks', 'search', 'new', 'runCommands', 'runTasks', 'usages', 'vscodeAPI', 'problems', 'changes', 'testFailure', 'openSimpleBrowser', 'fetch', 'githubRepo', 'extensions', 'runTests']
+---
+
+# Overview
+- This custom Ask chat mode is designed to assist with questions related to Svelte projects And Vite projects.
+- websites
+  - https://svelte.jp/
+  - https://ja.vite.dev/
---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<header class="header">
+  <a class="header-title" href="/">Svelte EC</a>
+  <nav>
+    <ul class="header-links">
+      <li>ようこそゲストさん</li>
+      <li><a href="/cart">カート(0)</a></li>
+    </ul>
+  </nav>
+</header>
+
+<article class="product">
+  <div class="product-main">
+    <div class="image-container">
+      <img
+        src="https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-1.png"
+        alt="『Svelte Guild 』"
+      />
+    </div>
+    <div>
+      <h2>Svelte Guild</h2>
+      <dl>
+        <dt>価格</dt>
+        <dd>3500円</dd>
+      </dl>
+      <div>
+        <button>カートに入れる</button>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <h3>関連商品</h3>
+    <ul>
+      <li>
+        <a href="/products/react-book">React Book</a>
+        - 3500円
+      </li>
+      <li>
+        <a href="/products/vue-book">Vue Book</a>
+        - 3500円
+      </li>
+      <li>
+        <a href="/products/angular-book">Angular Book</a>
+        - 3500円
+      </li>
+    </ul>
+  </footer>
+</article>
+
+<style>
+  :global(body) {
+    margin: 0;
+    background-color: #eee;
+    padding: 0;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 0 15px;
+    width: 100%;
+    max-width: 800px;
+    height: 50px;
+  }
+
+  .header-title {
+    font-weight: bold;
+  }
+
+  .header-links {
+    display: flex;
+    gap: 10px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .product {
+    margin: 0 auto;
+    background-color: #fff;
+    padding: 15px;
+    width: 100%;
+    max-width: 800px;
+  }
+
+  .product-main {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .image-container {
+    width: 100%;
+    max-width: 400px;
+    overflow: hidden;
+  }
+
+  .image-container img {
+    width: 100%;
+  }
+</style>
