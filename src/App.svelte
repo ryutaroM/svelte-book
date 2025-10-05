1813163a4caf7b7394c133a21ddaa0efcf61d5f7
ryutaroM <arcticmonkey326@gmail.com>
2025-10-05 22:37:57 +0900

    add slider function

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,53 @@
   function addToCart(productId) {
     cart = [...cart, productId];
   }
+
+  let product = {
+    id: "svelte-book",
+    name: "Svelte Guide",
+    price: 3500,
+    images: [
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-1.png",
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-2.png",
+      "https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-3.png",
+    ],
+  };
+
+  let relatedProducts = [
+    {
+      id: "react-book",
+      name: "React Book",
+      price: 3500,
+    },
+    {
+      id: "vue-book",
+      name: "Vue Book",
+      price: 3500,
+    },
+    {
+      id: "angular-book",
+      name: "Angular Book",
+      price: 3500,
+    },
+  ];
+
+  let sliderCenterIndex = 0;
+  let sliderLeftIndex = product.images.length - 1;
+  let sliderRightIndex = 1;
+
+  function sliderMoveLeft() {
+    const length = product.images.length;
+    sliderCenterIndex = (sliderCenterIndex - 1 + length) % length;
+    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
+    sliderRightIndex = (sliderCenterIndex + 1) % length;
+  }
+
+  function sliderMoveRight() {
+    const length = product.images.length;
+    sliderCenterIndex = (sliderCenterIndex + 1) % length;
+    sliderLeftIndex = (sliderCenterIndex - 1 + length) % length;
+    sliderRightIndex = (sliderCenterIndex + 1) % length;
+  }
 </script>
 
 <header class="header">
@@ -19,16 +66,32 @@
 <article class="product">
   <div class="product-main">
     <div class="image-container">
-      <img
-        src="https://github.com/svelte-book/sample-app/raw/main/static/svelte-book-1.png"
-        alt="『Svelte Guild 』"
-      />
+      <div class="slider">
+        <img
+          src={product.images[sliderLeftIndex]}
+          alt="スライダー画像（左）"
+          class="slider-item left"
+        />
+        <img
+          src={product.images[sliderCenterIndex]}
+          alt="スライダー画像（中央）"
+          class="slider-item"
+        />
+        <img
+          src={product.images[sliderRightIndex]}
+          alt="スライダー画像（右）"
+          class="slider-item right"
+        />
+        <button class="slider-left-button" on:click={sliderMoveLeft}>←</button>
+        <button class="slider-right-button" on:click={sliderMoveRight}>→</button
+        >
+      </div>
     </div>
     <div>
-      <h2>Svelte Guild</h2>
+      <h2>{product.name}</h2>
       <dl>
         <dt>価格</dt>
-        <dd>3500円</dd>
+        <dd>{product.price}円</dd>
       </dl>
       <div>
         {#if !cart.includes("svelte-book")}
@@ -45,18 +108,12 @@
   <footer>
     <h3>関連商品</h3>
     <ul>
-      <li>
-        <a href="/products/react-book">React Book</a>
-        - 3500円
-      </li>
-      <li>
-        <a href="/products/vue-book">Vue Book</a>
-        - 3500円
-      </li>
-      <li>
-        <a href="/products/angular-book">Angular Book</a>
-        - 3500円
-      </li>
+      {#each relatedProducts as product}
+        <li>
+          <a href="/products/{product.id}">{product.name}</a>
+          - {product.price}円
+        </li>
+      {/each}
     </ul>
   </footer>
 </article>
@@ -114,5 +171,39 @@
 
   .image-container img {
     width: 100%;
+  }
+
+  .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left {
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    left: 100%;
   }
 </style>
